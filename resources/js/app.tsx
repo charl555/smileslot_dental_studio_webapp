6a038ffbbc3a7f9f73eb0413694fc2bc624ea6f2
Charl <charldominicursabia@gmail.com>
2026-09-02 13:49:27 +0800

    first commit

---
+++ resources/js/app.tsx
@@ -0,0 +1,13 @@
+import { createInertiaApp } from '@inertiajs/react'
+import { createRoot } from 'react-dom/client'
+import type { ComponentType } from 'react'
+
+createInertiaApp({
+    resolve: (name) => {
+        const pages = import.meta.glob<{ default: ComponentType }>('./Pages/**/*.tsx', { eager: true })
+        return pages[`./Pages/${name}.tsx`]
+    },
+    setup({ el, App, props }) {
+        createRoot(el).render(<App {...props} />)
+    },
+})
